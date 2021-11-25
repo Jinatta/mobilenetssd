f92ddb27f654371ffbf5349ebff47739a1877ae1
Jinatta <89238027+Jinatta@users.noreply.github.com>
2021-11-25 11:11:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ def callback():
     no_event = len(decoded['events'])
     for i in range(no_event):
             event = decoded['events'][i]
-            event_handle(event,jason_line)
+            event_handle(event,json_line)
 
     # เชื่อมต่อกับ dialogflow
     #intent = decoded["queryResult"]["intent"]["displayName"] 
@@ -125,6 +125,7 @@ def reply(intent,text,reply_token,id,disname):
     line_bot_api.reply_message(reply_token,text_message)
 
 def event_handle(event,json_line):
+    print(event)
     try:
         userId = event['source']['userId']
     except:
